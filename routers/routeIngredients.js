d728e89ab0fbb8c326db2294da64b9bf8eee753a
HB <nelmstels@gmail.com>
2022-09-08 11:51:31 +0300

    Add dynamic id route to get ingredients by id

--- routers/routeIngredients.js
+++ routers/routeIngredients.js
@@ -4,6 +4,7 @@ const controller = require("../controllers/controllerIngredients");
 const router = Router();
 
 router.get("/", controller.getIngredients);
+router.get("/:id", controller.getIngredientById)
 router.post("/", controller.addIngredient);
 
 module.exports = router;
