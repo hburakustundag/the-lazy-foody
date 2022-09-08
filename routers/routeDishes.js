3a1e0dea4563bd57e23975cfb22bbb08c5fbe3da
HB <nelmstels@gmail.com>
2022-09-08 11:50:49 +0300

    Add dynamic id route to get dishes by id

--- routers/routeDishes.js
+++ routers/routeDishes.js
@@ -4,6 +4,7 @@ const controller = require("../controllers/controllerDishes");
 const router = Router();
 
 router.get("/", controller.getDishes);
+router.get("/:id", controller.getDishById);
 router.post("/", controller.addDishes);
 
 module.exports = router;
