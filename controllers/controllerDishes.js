941a506c5a4d623de89bec9ecfc9d08d01b5dce8
HB <nelmstels@gmail.com>
2022-09-08 10:48:24 +0300

    Add get and add functions for dishes and ingredients

--- controllers/controllerDishes.js
+++ controllers/controllerDishes.js
@@ -1,13 +1,30 @@
-const pool = require('../db')
-const queries = require('../src/queries');
+const pool = require("../db");
 
 const getDishes = (req, res) => {
-    pool.query("SELECT * FROM dishes", (error, results) => {
-      if (error) throw error;
-      res.status(200).json(results.rows);
-    });
-  };
+  pool.query("SELECT * FROM dishes", (error, results) => {
+    if (error) throw error;
+    res.status(200).json(results.rows);
+  });
+};
+
+const addDishes = async (req, res) => {
+  try {
+    const checkDishExists = "SELECT s FROM dishes s WHERE s.dish_name = $1";
+    const addDish = "INSERT INTO dishes (dish_name) VALUES ($1) RETURNING *";
+    const value = [req.body.dish_name];
+
+    const results = await pool.query(checkDishExists, value);
+    if (results.rows.length) {
+      res.send("Dish already exists.");
+    }
+    await pool.query(addDish, value);
+    res.status(201).send("The dish is added.");
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 module.exports = {
-    getDishes,
-}
+  getDishes,
+  addDishes,
+};
--- controllers/controllerIngredients.js
+++ controllers/controllerIngredients.js
@@ -1,12 +1,34 @@
-// const controller = require('../db');
-// const queries = require('../src/queries');
+const pool = require("../db");
 
-// const addIngredients = async (req, res) => {
+const getIngredients = async (req, res) => {
+  try {
+    const text = "SELECT * FROM ingredients";
+    const results = await pool.query(text);
 
-// }
+    res.status(200).send(results.rows);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
+const addIngredient = async (req, res) => {
+  try {
+    const checkIngredient = "SELECT s FROM ingredients s WHERE s.ingredient_name = $1";
+    const addIngredient = "INSERT INTO ingredients (ingredient_name) VALUES ($1) RETURNING *";
+    const value = [req.body.ingredient_name];
 
-// module.exports = {
-//     getIngredients,
-//     addIngredients,
-// }
+    const results = await pool.query(checkIngredient, value);
+    if (results.rows.length) {
+      res.send("Ingredient already exists.");
+    }
+    await pool.query(addIngredient, value);
+    res.status(201).send("The ingredient is successfully added.");
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+module.exports = {
+  getIngredients,
+  addIngredient,
+};
