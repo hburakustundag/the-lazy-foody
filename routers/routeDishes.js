7f77d4b1af0c41d8b7f64aeb832762b83c136a35
Halit Burak Ustundag <hburakustundag@gmail.com>
2022-09-14 08:49:07 +0300

    Create suggest post router

--- routers/routeDishes.js
+++ routers/routeDishes.js
@@ -6,5 +6,6 @@ const router = Router();
 router.get("/", controller.getDishes);
 router.get("/:id", controller.getDishById);
 router.post("/", controller.addDishes);
+router.post("/suggest", controller.suggest);
 
 module.exports = router;
