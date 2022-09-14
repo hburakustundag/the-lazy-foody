f2c8ba9c16ca937880d791613b6328182f04657b
Halit Burak Ustundag <hburakustundag@gmail.com>
2022-09-14 08:47:19 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: "halit",
+    user: "halitburak",
     host: "localhost",
     database: "the-lazy-foody",
     password: "test",
