842a5747c7a46568e6d2a5a5ef005aa9155d9e6f
Halit Burak Ustundag <hburakustundag@gmail.com>
2022-09-14 08:46:29 +0300

    Add suggest function to get suggested dishes

--- controllers/controllerDishes.js
+++ controllers/controllerDishes.js
@@ -36,8 +36,27 @@ const addDishes = async (req, res) => {
   }
 };
 
+const suggest = async (req, res) => {
+  try {
+    const dish_names = req.body.dish_names;
+    const dish_count = req.body.dish_names.length;
+    const suggestionQuery = "SELECT dish_name\n" +
+        "FROM ingredients\n" +
+        "join dish_ingredient di on ingredients.id = di.ingredient_id\n" +
+        "join dishes d on di.dish_id = d.id\n" +
+        "where ingredient_name = ANY ($1)\n" +
+        "group by dish_name\n" +
+        "having COUNT(*) = $2"
+    const getResult = await pool.query(suggestionQuery, [dish_names, dish_count]);
+    res.json(getResult.rows);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
 module.exports = {
   getDishes,
   addDishes,
   getDishById,
+  suggest
 };
