007a164491e0e3b12205ccf607c8f3b0008587ef
Halit Burak Ustundag <hburakustundag@gmail.com>
2022-09-23 15:58:49 +0300

    Add validators

--- routers/routeDishes.js
+++ routers/routeDishes.js
@@ -1,11 +1,13 @@
 const { Router } = require("express");
 const controller = require("../controllers/controllerDishes");
+const validator = require("../validations/validator")
 
 const router = Router();
 
 router.get("/", controller.getDishes);
-router.get("/:id", controller.getDishById);
-router.post("/", controller.addDishes);
+router.get("/:id", validator.getDishById, controller.getDishById);
+router.post("/", validator.addDishes, controller.addDishes);
 router.post("/suggest", controller.suggest);
+router.delete('/:id', controller.removeDishes)
 
 module.exports = router;
--- routers/routeIngredients.js
+++ routers/routeIngredients.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
 const controller = require("../controllers/controllerIngredients");
+const validator = require("../validations/validator")
 
 const router = Router();
 
 router.get("/", controller.getIngredients);
-router.get("/:id", controller.getIngredientById)
-router.post("/", controller.addIngredient);
+router.get("/:id", validator.getIngredientById, controller.getIngredientById)
+router.post("/", validator.addIngredient, controller.addIngredient);
 
 module.exports = router;
