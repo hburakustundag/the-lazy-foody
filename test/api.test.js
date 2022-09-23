2f83b707aba2d9b88bdc92f57bd4d5d528f4ea01
Halit Burak Ustundag <hburakustundag@gmail.com>
2022-09-23 15:54:18 +0300

    Add unit test for delete request

---
+++ test/api.test.js
@@ -0,0 +1,28 @@
+const request = require("supertest");
+const app = require('../server')
+const httpStatus = require('http-status')
+const { Pool } = require('pg')
+
+jest.mock('pg', () => {
+    const mClient = {
+        connect: jest.fn(),
+        query: jest.fn(),
+        end: jest.fn(),
+    };
+    return { Pool: jest.fn(() => mClient) };
+});
+
+describe("DELETE /dishes", () => {
+
+    beforeEach(() => {
+        app.pool = new Pool();
+    });
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+    it('should delete dish', () => {
+        return request(app)
+            .delete('/dishes/example')
+            .expect(httpStatus.NO_CONTENT);
+    })
+})
