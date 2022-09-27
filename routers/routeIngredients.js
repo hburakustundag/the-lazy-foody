880862a56ee0ab1d5e838f7964506d0193d53bd6
Halit Burak Ustundag <hburakustundag@gmail.com>
2022-09-27 20:48:26 +0300

    Add delete router

--- routers/routeIngredients.js
+++ routers/routeIngredients.js
@@ -7,5 +7,6 @@ const router = Router();
 router.get("/", controller.getIngredients);
 router.get("/:id", validator.getIngredientById, controller.getIngredientById)
 router.post("/", validator.addIngredient, controller.addIngredient);
+router.delete("/:id", controller.removeIngredient)
 
 module.exports = router;
