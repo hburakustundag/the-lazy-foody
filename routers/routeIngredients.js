9da845208bb41edbfaba5f09cccf48b74a87d55d
Halit Burak Ustundag <hburakustundag@gmail.com>
2022-09-29 13:02:51 +0300

    Update routeIngredients.js

--- routers/routeIngredients.js
+++ routers/routeIngredients.js
@@ -1,12 +1,12 @@
 const { Router } = require("express");
 const controller = require("../controllers/controllerIngredients");
-const validator = require("../validations/validator")
+const validator = require("../validations/validator");
 
 const router = Router();
 
 router.get("/", controller.getIngredients);
-router.get("/:id", validator.getIngredientById, controller.getIngredientById)
+router.get("/:id", validator.getIngredientById, controller.getIngredientById);
 router.post("/", validator.addIngredient, controller.addIngredient);
-router.delete("/:id", controller.removeIngredient)
+router.delete("/:id", controller.removeIngredient);
 
 module.exports = router;
