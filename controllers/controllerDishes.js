9a1f2ce4aa9a4d124481bf45b809f2cec8830757
Halit Burak Ustundag <hburakustundag@gmail.com>
2022-09-29 13:04:09 +0300

    Create cache for getDishById

--- controllers/controllerDishes.js
+++ controllers/controllerDishes.js
@@ -1,11 +1,14 @@
 const db = require("../db/db");
 const database = new db();
+const queries = require("./queries");
 
-const getDishes = (req, res) => {
-  database.pool.query("SELECT * FROM dishes", (error, results) => {
-    if (error) throw error;
-    res.status(200).json(results.rows);
-  });
+const getDishes = async (req, res) => {
+  try {
+    const getAllDishes = await database.pool.query(queries.getAllDishes);
+    res.status(200).json(getAllDishes.rows);
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
 const getDishById = async (req, res) => {
@@ -16,50 +19,25 @@ const getDishById = async (req, res) => {
       console.log("returned from cache");
       return res.json(JSON.parse(getDishFromCache));
     }
-    const getDish = "SELECT * FROM dishes WHERE id = $1";
-    const getDishResult = await database.pool.query(getDish, [id]);
-    const getDishFirstRow = JSON.stringify(getDishResult.rows[0]);
-
-    await database.redisClient.set(`dish:${id}`, getDishFirstRow);
-
-    return res.json(getDishResult.rows[0]);
-  } catch (error) {}
+    const getOneDish = await database.pool.query(queries.getOneDish, [id]);
+    const stringifyGetOneDish = JSON.stringify(getOneDish.rows[0]);
+    await database.redisClient.setEx(`dish:${id}`, 300, stringifyGetOneDish);
+    res.json(getOneDish.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
 const addDishes = async (req, res) => {
   try {
-    const checkDishExists = "SELECT s FROM dishes s WHERE s.dish_name = $1";
-    const addDish = "INSERT INTO dishes (dish_name) VALUES ($1) RETURNING *";
-    const value = [req.body.dish_name];
-
-    const results = await database.pool.query(checkDishExists, value);
-    if (results.rows.length) {
+    const checkDishQueryResult = await database.pool.query(
+      queries.checkDishExists,
+    );
+    if (checkDishQueryResult.rows.length) {
       res.send("Dish already exists.");
     }
-    await database.pool.query(addDish, value);
+    await database.pool.query(queries.addDish, [req.body.dish_name]);
     res.status(201).send("The dish is added.");
-  } catch (error) {
-    console.log(error.message);
-  }
-};
-
-const suggest = async (req, res) => {
-  try {
-    const dish_names = req.body.dish_names;
-    const dish_count = req.body.dish_names.length;
-    const suggestionQuery =
-      "SELECT dish_name\n" +
-      "FROM ingredients\n" +
-      "join dish_ingredient di on ingredients.id = di.ingredient_id\n" +
-      "join dishes d on di.dish_id = d.id\n" +
-      "where ingredient_name = ANY ($1)\n" +
-      "group by dish_name\n" +
-      "having COUNT(*) = $2";
-    const getResult = await database.pool.query(suggestionQuery, [
-      dish_names,
-      dish_count,
-    ]);
-    res.json(getResult.rows);
   } catch (error) {
     console.error(error.message);
   }
@@ -67,10 +45,23 @@ const suggest = async (req, res) => {
 
 const removeDish = async (req, res) => {
   try {
-    const removeDish = "DELETE FROM dishes WHERE id = $1";
     const { id } = req.params;
-    await database.pool.query(removeDish, [id]);
+    await database.pool.query(queries.removeDish, [id]);
     res.status(200).send({ message: "Successfully removed" });
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+const suggestDishes = async (req, res) => {
+  try {
+    const dish_names = req.body.dish_names;
+    const dish_count = req.body.dish_names.length;
+    const suggestDishes = await database.pool.query(queries.suggestDishes, [
+      dish_names,
+      dish_count,
+    ]);
+    res.json(suggestDishes.rows);
   } catch (error) {
     console.error(error.message);
   }
@@ -80,6 +71,6 @@ module.exports = {
   getDishes,
   addDishes,
   getDishById,
-  suggest,
+  suggestDishes,
   removeDish,
 };
