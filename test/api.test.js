ed694fd1f68f47cd2ce2d5982852597cb05cc91d
Halit Burak Ustundag <hburakustundag@gmail.com>
2022-09-29 13:01:45 +0300

    Format api.test.js

--- test/api.test.js
+++ test/api.test.js
@@ -1,28 +1,25 @@
 const request = require("supertest");
-const app = require('../server')
-const httpStatus = require('http-status')
-const { Pool } = require('pg')
+const app = require("../server");
+const httpStatus = require("http-status");
+const { Pool } = require("pg");
 
-jest.mock('pg', () => {
-    const mClient = {
-        connect: jest.fn(),
-        query: jest.fn(),
-        end: jest.fn(),
-    };
-    return { Pool: jest.fn(() => mClient) };
+jest.mock("pg", () => {
+  const mClient = {
+    connect: jest.fn(),
+    query: jest.fn(),
+    end: jest.fn(),
+  };
+  return { Pool: jest.fn(() => mClient) };
 });
 
 describe("DELETE /dishes", () => {
-
-    beforeEach(() => {
-        app.pool = new Pool();
-    });
-    afterEach(() => {
-        jest.clearAllMocks();
-    });
-    it('should delete dish', () => {
-        return request(app)
-            .delete('/dishes/example')
-            .expect(httpStatus.NO_CONTENT);
-    })
-})
+  beforeEach(() => {
+    app.pool = new Pool();
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  it("should delete dish", () => {
+    return request(app).delete("/dishes/example").expect(httpStatus.NO_CONTENT);
+  });
+});
