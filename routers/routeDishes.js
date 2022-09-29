97eb841d33063f03e90f0f4a550931003695476f
Halit Burak Ustundag <hburakustundag@gmail.com>
2022-09-29 12:59:23 +0300

    Update routeDishes.js

--- routers/routeDishes.js
+++ routers/routeDishes.js
@@ -1,13 +1,13 @@
 const { Router } = require("express");
 const controller = require("../controllers/controllerDishes");
-const validator = require("../validations/validator")
+const validator = require("../validations/validator");
 
 const router = Router();
 
 router.get("/", controller.getDishes);
 router.get("/:id", validator.getDishById, controller.getDishById);
 router.post("/", validator.addDishes, controller.addDishes);
-router.post("/suggest", controller.suggest);
-router.delete('/:id', controller.removeDish)
+router.post("/suggest", controller.suggestDishes);
+router.delete("/:id", controller.removeDish);
 
 module.exports = router;
