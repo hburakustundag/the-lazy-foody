71e25820714685da2bc80265423ad355caf58cfd
Halit Burak Ustundag <hburakustundag@gmail.com>
2022-10-18 14:23:22 +0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, {Request, Response} from 'express' ;
 const morgan = require("morgan");
 const app = express();
+const controller = require('./controllers/controllerIngredients')
 
 const port = process.env.PORT || 3000;
 
@@ -8,16 +9,16 @@ const routeDishes = require('./routers/routeDishes');
 const routeIngredients = require('./routers/routeIngredients');
 
 app.use(express.json());
-
+app.set('view engine', 'ejs');
 app.use(morgan("combined"));
 
+app.use(express.urlencoded({extended: true}))
 app.use("/ingredients", routeIngredients);
 app.use("/dishes", routeDishes);
+app.use("/", express.static('public'))
 
 
-app.get("/", (req, res) => {
-  res.send("Hello, welcome!");
-});
+app.get("/", controller.getIngredients);
 
 app.listen(port, () => {
   console.log(`app is listening on port ${port}`);
