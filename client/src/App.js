c53e9f1a81da36cb5dbb579df8e0455d6fddc637
Halit Burak Ustundag <hburakustundag@gmail.com>
2022-10-21 17:43:31 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
-import React, { Fragment } from 'react';
-import './App.css';
-import ListIngredients from './components/ListIngredients';
+import React, { Fragment } from "react";
+import "./App.css";
+import ListIngredients from "./components/ListIngredients";
 
 function App() {
-  return <Fragment>
-    <ListIngredients />
-  </Fragment>;
+  return (
+    <Fragment>
+      <ListIngredients />
+    </Fragment>
+  );
 }
 
 export default App;
