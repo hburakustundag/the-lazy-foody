dc45c4be06b37f48724c5534d572c69e2bf6a8a8
Halit Burak Ustundag <hburakustundag@gmail.com>
2022-10-31 21:49:13 +0300

    Add loginUser

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -11,9 +11,9 @@ const registerUser = async (req: Request, res: Response) => {
     const checkUser = await database.pool.query(queries.checkUserExists, [
       username,
     ]);
-    if (checkUser.rows) {
+    if (checkUser.rows.length) {
       console.log("You can't take this username.");
-      res.status(400).json("You can't take this username.");
+      res.send("You can't take this username.");
     } else {
       await database.pool.query(queries.registerUser, [username, password]);
       res.status(201).send("User is successfully created.");
@@ -23,4 +23,22 @@ const registerUser = async (req: Request, res: Response) => {
   }
 };
 
-export { registerUser };
+const loginUser = async (req: Request, res: Response) => {
+  try {
+    const username = req.body.username;
+    const password = md5(req.body.password);
+    const loggedIn = await database.pool.query(queries.loginUser, [
+      username,
+      password,
+    ]);
+    if (loggedIn.rows.length) {
+      res.status(200).send("Succesfully logged in.");
+    } else {
+      res.status(200).send("Wrong username or password.");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { registerUser, loginUser };
