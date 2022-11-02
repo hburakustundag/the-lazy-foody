fe769f1783b431a3ca81efed107591faa630dbd4
Halit Burak Ustundag <hburakustundag@gmail.com>
2022-11-02 18:43:19 +0300

    Add authorize function as middleware

--- src/routers/routeDishes.ts
+++ src/routers/routeDishes.ts
@@ -1,16 +1,14 @@
 import { Router } from "express";
-const controller = require ('../controllers/controllerDishes.ts')
+const controller = require("../controllers/controllerDishes.ts");
 const validator = require("../helpers/validator");
+import authorize from "../helpers/authorizationMiddleware";
 
 const router = Router();
 
 router.get("/", controller.getDishes);
 router.get("/:id", validator.getDishById, controller.getDishById);
-router.post("/", validator.addDishes, controller.addDishes);
+router.post("/", validator.addDishes, authorize(), controller.addDishes);
 router.post("/suggest", controller.suggestDishes);
-router.delete("/:id", controller.removeDish);
+router.delete("/:id", authorize(), controller.removeDish);
 
 module.exports = router;
-
-
-
