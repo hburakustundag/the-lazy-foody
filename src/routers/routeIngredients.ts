d92a03f64716c30bb4a633e9c71a4b7a1c0bf23e
Halit Burak Ustundag <hburakustundag@gmail.com>
2022-11-02 18:43:45 +0300

    Add authorize function as middleware

--- src/routers/routeIngredients.ts
+++ src/routers/routeIngredients.ts
@@ -1,12 +1,17 @@
 import { Router } from "express";
 const controller = require("../controllers/controllerIngredients");
 const validator = require("../helpers/validator");
-
+import authorize from "../helpers/authorizationMiddleware";
 const router = Router();
 
 router.get("/", controller.getIngredients);
 router.get("/:id", validator.getIngredientById, controller.getIngredientById);
-router.post("/", validator.addIngredient, controller.addIngredient);
-router.delete("/:id", controller.removeIngredient);
+router.post(
+  "/",
+  validator.addIngredient,
+  authorize(),
+  controller.addIngredient
+);
+router.delete("/:id", authorize(), controller.removeIngredient);
 
 module.exports = router;
